25824e2c3086db8fd85af568898b918aa06f5861
vita-dounai <lichenxi.webank@gmail.com>
2020-08-11 17:56:59 +0800

    add default_accounts test api

--- core/src/env/engine/off_chain/test_api.rs
+++ core/src/env/engine/off_chain/test_api.rs
@@ -11,7 +11,10 @@
 // limitations under the License.
 
 use super::{EnvInstance, ExecContext};
-use crate::env::{engine::OnInstance, types::Address};
+use crate::env::{
+    engine::OnInstance,
+    types::{Address, ADDRESS_LENGTH},
+};
 
 /// Pushes a contract execution context.
 ///
@@ -37,4 +40,27 @@ pub fn pop_execution_context() {
     <EnvInstance as OnInstance>::on_instance(|instance| {
         instance.exec_contexts.pop();
     })
+}
+
+/// The default accounts.
+pub struct DefaultAccounts {
+    pub alice: Address,
+    pub bob: Address,
+    pub charlie: Address,
+    pub david: Address,
+    pub eva: Address,
+    pub frank: Address,
+}
+
+/// Returns the default accounts for testing purposes:
+/// Alice, Bob, Charlie, Django, Eve and Frank
+pub fn default_accounts() -> DefaultAccounts {
+    DefaultAccounts {
+        alice: Address::from_bytes(&[0x00; ADDRESS_LENGTH]),
+        bob: Address::from_bytes(&[0x01; ADDRESS_LENGTH]),
+        charlie: Address::from_bytes(&[0x02; ADDRESS_LENGTH]),
+        david: Address::from_bytes(&[0x03; ADDRESS_LENGTH]),
+        eva: Address::from_bytes(&[0x04; ADDRESS_LENGTH]),
+        frank: Address::from_bytes(&[0x05; ADDRESS_LENGTH]),
+    }
 }
