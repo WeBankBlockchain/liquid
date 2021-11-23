99e9b05b1ba86d9c71afbbcc0d4304a78eeb8b82
xueying4402 <1120859481@qq.com>
2021-11-23 14:05:50 +0800

    complete simple fixed point

--- primitives/src/types/fixed_point_simple.rs
+++ primitives/src/types/fixed_point_simple.rs
@@ -11,12 +11,12 @@
 // limitations under the License.
 
 #[allow(unused_imports)]
-use core::{default::Default, ops::{Add, AddAssign, Div, DivAssign, Deref, Mul, MulAssign, Sub, SubAssign}, panic, str::FromStr};
+use core::{default::Default, ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign}, panic, str::FromStr, cmp::Ordering, fmt, ops::Shr};
 use liquid_prelude::vec::{from_elem, Vec};
 pub use fixed::{FixedU64, types::extra::{U16}};
+use num::{Bounded, CheckedAdd};
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
-#[cfg_attr(feature = "std", derive(Debug))]
+#[derive(Copy, Clone, PartialEq, Eq, Default)]
 pub struct FixedPointU64F16{
     symbol: u8, 
     value: FixedU64<U16>,
@@ -47,13 +47,102 @@ impl FixedPointU64F16 {
         if ans.value == FixedU64::<U16>::from_num(0){
             ans.symbol = 0;
         }
-        if ans.value > FixedU64::<U16>::from_num(1099511627775.99998) {
+        if ans > FixedPointU64F16:: max_value() {
             panic!("the value exceed MAX")
         }
-        // println!("{:?}", ans);     
         ans
     }
 }
+
+impl core::ops::Shl<usize> for FixedPointU64F16 {
+    type Output = Self;
+
+    fn shl(self, rhs: usize) -> Self::Output {
+        Self {
+            symbol: self.symbol,
+            value: self.value << rhs,
+        }
+    }
+}
+
+impl Shr<usize> for FixedPointU64F16 {
+    type Output = Self;
+
+    fn shr(self, rhs: usize) -> Self::Output {
+        Self {
+            symbol: self.symbol,
+            value: self.value >> rhs,
+        }
+    }
+}
+
+
+impl Bounded for FixedPointU64F16 {
+    fn min_value() -> Self {
+        FixedPointU64F16 {
+            symbol: 255,
+            value: FixedU64::<U16>::from_num(1099511627775.99998)
+        }
+    }
+
+    fn max_value() -> Self {
+        FixedPointU64F16 { 
+            symbol: 0, 
+            value: FixedU64::<U16>::from_num(1099511627775.99998) 
+        }
+    }
+}
+
+impl Ord for FixedPointU64F16 {
+    fn cmp(&self, other: &Self) -> core::cmp::Ordering {
+        if self.symbol > other.symbol {
+            return Ordering::Less;
+        }
+        else if self.symbol < other.symbol {
+            return Ordering::Greater;
+        }
+        else if self.symbol == 0 {
+            return self.value.cmp(&other.value)
+        }
+        else {
+            return other.value.cmp(&self.value)
+        }
+    }
+}
+
+impl PartialOrd for FixedPointU64F16 {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl fmt::Display for FixedPointU64F16 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut symbol = "";
+        if self.symbol == 255 {
+            symbol = "-";
+        }
+        write!(f, "{}{}", &symbol, self.value)
+    }
+}
+
+impl fmt::Debug for FixedPointU64F16 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut symbol = "";
+        if self.symbol == 255 {
+            symbol = "-";
+        }
+        write!(f, "FixedPointU64F16({}{})", &symbol, self.value)
+    }
+}
+
+// impl Deref for FixedPointU64F16 {
+//     type Target = FixedPointU64F16;
+
+//     fn deref(&self) -> &Self::Target {
+//         &self
+//     }
+// }
 
 
 impl Add for FixedPointU64F16 {
@@ -73,18 +162,19 @@ impl Add for FixedPointU64F16 {
             ans.symbol = rhs.symbol;
             ans.value = rhs.value - ans.value;
         }
-        return ans;
+        if ans > FixedPointU64F16::max_value() {
+            panic!("the value exceed MAX")
+        }
+        ans
     }
 }
 
-
-// impl AddAssign for FixedPointU64F16{
-//     fn add_assign(&mut self, rhs: Self) {
-//         let mut a = self;
-//         a = &mut a.add(rhs);
-//         // println!("haha: {:?}", a);
-//     }
-// }
+impl CheckedAdd for FixedPointU64F16 {
+    fn checked_add(&self, v: &Self) -> Option<Self> {
+        let a = self;
+        Some(a.add(*v))
+    }
+}
 
 impl Sub for FixedPointU64F16 {
     type  Output = FixedPointU64F16;
@@ -104,66 +194,52 @@ impl Sub for FixedPointU64F16 {
             ans.symbol = rhs.symbol;
             ans.value = rhs.value - ans.value;
         }
-        return ans;
+        ans
     }
 }
 
 // impl SubAssign for FixedPointU64F16 {
 //     fn sub_assign(&mut self, rhs: Self) {
-//         let a = &self;
-//         a.sub(rhs);
+//         // copy trait???
+//         // let mut a = self;
+//         // (*self) = a.sub(rhs);
+//         *self = self.sub(rhs);
 //     }
 // }
 
-impl Mul for FixedPointU64F16{
-    type Output = FixedPointU64F16;
-    fn mul(self, rhs: Self) -> Self::Output {
-        let mut ans = self;
-        ans.symbol = ans.symbol | rhs.symbol;
-        ans.value = ans.value.mul(rhs.value);
-        return ans;
-    }    
+macro_rules! forward_op {
+    (impl $trait_: ident for $type_:ident {fn $method:ident}) => {
+        impl $trait_ for $type_{
+            type Output = $type_;
+            fn $method(self, b: $type_) -> $type_ {
+                let mut ans = self;
+                ans.symbol = ans.symbol ^ b.symbol;
+                ans.value = ans.value.$method(b.value);
+                ans
+            }
+        }
+    };
 }
+forward_op!{impl Mul for FixedPointU64F16 { fn mul} }
+forward_op!{impl Div for FixedPointU64F16 { fn div} }
 
-// impl MulAssign for FixedPointU64F16 {
-//     fn mul_assign(&mut self, rhs: Self) {
-//         let ans = &self;
-//         ans.mul(rhs);
-//     }
-// }
 
-impl Div for FixedPointU64F16 {
-    type Output = FixedPointU64F16;
-    fn div(self, rhs: Self) -> Self::Output {
-        let mut ans = self;
-        ans.symbol = ans.symbol | rhs.symbol;
-        ans.value = ans.value.div(rhs.value);
-        return ans;
+macro_rules! forward_assign_op {
+    (impl $trait_: ident for $type_: ident { fn $method: ident} $methodinvoke: ident) => {
+        impl $trait_ for $type_{
+            fn $method(&mut self, rhs: Self) {
+                *self = self.$methodinvoke(rhs)
+            }
+        }
     }
 }
 
-// impl DivAssign for FixedPointU64F16 {
-//     fn div_assign(&mut self, rhs: Self) {
-//         let ans = &self;
-//         ans.div(rhs);
-//     }
-// }
+forward_assign_op!{impl AddAssign for FixedPointU64F16 { fn add_assign} add}
+forward_assign_op!{impl SubAssign for FixedPointU64F16 { fn sub_assign} sub}
+forward_assign_op!{impl MulAssign for FixedPointU64F16 { fn mul_assign} mul}
+forward_assign_op!{impl DivAssign for FixedPointU64F16 { fn div_assign} div}
 
 
-// macro_rules! forward_op {
-//     (impl $trait_: ident for $type_:ident {fn $method:ident}) => {
-//         impl $trait_ for $type_{
-//             type Output = $type_;
-//             fn $method(self, $type_(b): $type_) -> $type_ {
-//                 let $type_(ans) = self;
-//                 ans.symbol = ans.symbol | b.symbol;
-//                 ans.value = ans.value.$method(b.value);
-//                 $type_(ans)
-//             }
-//         }
-//     };
-// }
-// forward_op!{impl Mul for FixedPointU64F16 { fn mul} }
 
 
 impl scale::Encode for FixedPointU64F16 {
@@ -190,7 +266,6 @@ mod tests {
     #[test]
     fn fixed_point_encode1() {
         let origin2:FixedPointU64F16 = FixedPointU64F16::from_user("12345678.987");
-        println!("{:?}", origin2);
         let encoded = scale::Encode::encode(&origin2);
         assert_eq!(encoded, [0,0,0,0,188,97,78,252,172]);
     }
@@ -198,7 +273,6 @@ mod tests {
     #[test]
     fn fixed_point_encode2() {
         let origin2:FixedPointU64F16 = FixedPointU64F16::from_user("-0");
-        println!("{:?}", origin2);
         let encoded = scale::Encode::encode(&origin2);
         assert_eq!(encoded, [0,0,0,0,0,0,0,0,0]);
     }
@@ -206,7 +280,6 @@ mod tests {
     #[test]
     fn fixed_point_encode3() {
         let origin2:FixedPointU64F16 = FixedPointU64F16::from_user("0");
-        println!("{:?}", origin2);
         let encoded = scale::Encode::encode(&origin2);
         assert_eq!(encoded, [0,0,0,0,0,0,0,0,0]);
     }
@@ -214,9 +287,7 @@ mod tests {
     #[test]
     fn fixed_point_codec1() {
         let origin2:FixedPointU64F16 = FixedPointU64F16::from_user("-0");
-        println!("{:?}", origin2);
         let encoded = scale::Encode::encode(&origin2);
-        println!("{:?}", encoded);
         let decoded: FixedPointU64F16  = scale::Decode::decode(&mut encoded.as_slice()).unwrap();
         assert_eq!(origin2, decoded);
     }
@@ -224,9 +295,7 @@ mod tests {
     #[test]
     fn fixed_point_codec2() {
         let origin2:FixedPointU64F16 = FixedPointU64F16::from_user("0");
-        println!("{:?}", origin2);
         let encoded = scale::Encode::encode(&origin2);
-        println!("{:?}", encoded);
         let decoded: FixedPointU64F16  = scale::Decode::decode(&mut encoded.as_slice()).unwrap();
         assert_eq!(origin2, decoded);
     }
@@ -234,9 +303,7 @@ mod tests {
     #[test]
     fn fixed_point_codec3() {
         let origin2:FixedPointU64F16 = FixedPointU64F16::from_user("1");
-        println!("{:?}", origin2);
         let encoded = scale::Encode::encode(&origin2);
-        println!("{:?}", encoded);
         let decoded: FixedPointU64F16  = scale::Decode::decode(&mut encoded.as_slice()).unwrap();
         assert_eq!(origin2, decoded);
     }
@@ -244,9 +311,7 @@ mod tests {
     #[test]
     fn fixed_point_codec4() {
         let origin2:FixedPointU64F16 = FixedPointU64F16::from_user("1099511627775.99998");
-        println!("{:?}", origin2);
         let encoded = scale::Encode::encode(&origin2);
-        println!("{:#?}", encoded);
         let decoded: FixedPointU64F16  = scale::Decode::decode(&mut encoded.as_slice()).unwrap();
         assert_eq!(origin2, decoded);
     }
@@ -254,9 +319,7 @@ mod tests {
     #[test]
     fn fixed_point_codec5() {
         let origin2:FixedPointU64F16 = FixedPointU64F16::from_user("1.25");
-        println!("{:?}", origin2);
         let encoded = scale::Encode::encode(&origin2);
-        println!("{:#?}", encoded);
         let decoded: FixedPointU64F16  = scale::Decode::decode(&mut encoded.as_slice()).unwrap();
         assert_eq!(origin2, decoded);
     }
@@ -269,12 +332,48 @@ mod tests {
         assert_eq!(result, FixedPointU64F16 { symbol: 0, value: FixedU64::<U16>::from_num(0.9)});
     }
 
-    // #[test]
-    // fn fixed_point_add_assign1() {
-    //     let origin2:FixedPointU64F16 = FixedPointU64F16::from_user("-1.25");
-    //     let mut origin = FixedPointU64F16::from_user("2.15");
-    //     origin.add_assign(origin2);
-    //     println!("{:?}", origin);
-    //     assert_eq!(origin, FixedPointU64F16 { symbol: 0, value: FixedU64::<U16>::from_num(0.9)});
-    // }
+    #[test]
+    fn fixed_point_add_assign1() {
+        let origin2:FixedPointU64F16 = FixedPointU64F16::from_user("-1.25");
+        let mut origin = FixedPointU64F16::from_user("-2.15");
+        origin += origin2;
+        assert_eq!(origin, FixedPointU64F16::from_user("-3.4"));
+    }
+
+    #[test]
+    fn fixed_point_mul1() {
+        let origin2:FixedPointU64F16 = FixedPointU64F16::from_user("-1.2");
+        let mut origin = FixedPointU64F16::from_user("2.1");
+        origin = origin.mul(origin2);
+        // 多么不准确的实现
+        assert_eq!(origin, FixedPointU64F16::from_user("-2.51999"));
+    }
+
+    #[test]
+    fn fixed_point_mul_assign1() {
+        let origin2:FixedPointU64F16 = FixedPointU64F16::from_user("-1.2");
+        let mut origin = FixedPointU64F16::from_user("-2.1");
+        origin *= origin2;
+        // 多么不准确的实现
+        assert_eq!(origin, FixedPointU64F16::from_user("2.51999"));
+    }
+
+    #[test]
+    fn fixed_point_cmp1() {
+        let origin2:FixedPointU64F16 = FixedPointU64F16::from_user("1.2");
+        let origin = FixedPointU64F16::from_user("-2.1");
+        assert_eq!(origin.cmp(&origin2), Ordering::Less)
+    }
+
+    #[test]
+    fn fixed_point_shl1() {
+        let origin2:FixedPointU64F16 = FixedPointU64F16::from_user("1.25");
+        assert_eq!(origin2<<1, FixedPointU64F16::from_user("2.5"))
+    }
+
+    #[test]
+    fn fixed_point_shr1() {
+        let origin2:FixedPointU64F16 = FixedPointU64F16::from_user("1.25");
+        assert_eq!(origin2>>1, FixedPointU64F16::from_user("0.625"))
+    }
 }
